ce16540b1b791549376b5144a1bac6912faba2bc
Benjamin Cronin <bcronin720@gmail.com>
2020-02-07 16:31:28 -0800

    fix Brake() function in motor.h

--- WalnutBot/include/motors.h
+++ WalnutBot/include/motors.h
@@ -47,6 +47,8 @@ void Motor::InitMotor(vex::motor m, double wheel_circumference, double wheel_gea
 	this->wheel_circumference = wheel_circumference;
 	this->wheel_gear_ratio = wheel_gear_ratio;
 	this->wheel_turning_diameter = wheel_turning_diameter;
+  this->velocity_goal = 0;
+  this->velocity = 0;
 }
 
 void Motor::ChangeVelocity(double dt, double velocity_goal, vex::directionType direction) {
@@ -65,6 +67,9 @@ void Motor::UpdateVelocity() {
 }
 
 void Motor::Brake() {
+  this->velocity_goal = 0;
+  this->velocity = 0;
+  this->m->setVelocity(0, vex::velocityUnits::pct);
 	this->m->stop(vex::brakeType::hold);
 }
 
