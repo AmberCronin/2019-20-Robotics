354387ffe9c3b0f606fc8b8e889827443d67e37d
Benjamin Cronin <bcronin720@gmail.com>
2020-02-14 16:38:19 -0800

    update utility/boilerplate files

--- Libdev-Boilerplates/clawbot.h
+++ Libdev-Boilerplates/clawbot.h
@@ -16,10 +16,36 @@ private:
 	Motor l_claw_motor;
 	Motor r_claw_motor;
 public:
-	ArmsNClaw(std::vector<Motor> arm_motors, Motor l_claw_motor, Motor r_claw_motor);
+	void InitArmsNClaw(std::vector<Motor> arm_motors, Motor l_claw_motor, Motor r_claw_motor);
 	void MoveArms(double dt, double velocity_goal, vex::directionType direction);
 	void MoveClaws(double dt, double velocity_goal, vex::directionType direction);
 	void UpdateArmsNClaw();
+};
+
+void ArmsNClaw::InitArmsNClaw(std::vector<Motor> arm_motors, Motor l_claw_motor, Motor r_claw_motor) {
+	this->arm_motors = arm_motors;
+	this->l_claw_motor = l_claw_motor;
+	this->r_claw_motor = r_claw_motor;
+}
+
+void ArmsNClaw::MoveArms(double dt, double velocity_goal, vex::directionType direction) {
+	for (int i = 0; i < this->arm_motors.size(); ++i) {
+		this->arm_motors[i].ChangeVelocity(dt, velocity_goal, direction);
+	}
+}
+
+void ArmsNClaw::MoveClaws(double dt, double velocity_goal, vex::directionType direction) {
+	l_claw_motor.ChangeVelocity(dt, velocity_goal, directionType::fwd);
+	r_claw_motor.ChangeVelocity(dt, velocity_goal, directionType::rev);
+}
+
+void ArmsNClaw::UpdateArmsNClaw() {
+	this->l_claw_motor.UpdateVelocity();
+	this->r_claw_motor.UpdateVelocity();
+
+	for (int i = 0; i < this->arm_motors.size(); ++i) {
+		this->arm_motors[i].UpdateVelocity();
+	}
 }
 
 #endif // CLAWBOT_H_FILE
--- Libdev-Boilerplates/drive.h
+++ Libdev-Boilerplates/drive.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include "vex.h"
 
+#include "utils.h"
+
 /*
 // define your global instances of motors and other devices like this
 vex::motor      LBMotor(vex::PORT4, false);   //
@@ -12,27 +14,112 @@ vex::motor      RBMotor(vex::PORT3, true);    //
 vex::motor      RFMotor(vex::PORT1, true);    //
 */
 
+typedef struct wheel_data {
+  double wheel_circumference;
+  double wheel_gear_ratio;
+  double wheel_turning_diameter;
+} WheelData;
+
 using namespace vex;
 
 double Approach(double current, double goal, double dt);
 
 class Drive {
 private:
-	vex::motor LBMotor;
-	vex::motor LFMotor;
-	vex::motor RBMotor;
-	vex::motor RFMotor;
+	vex::motor* LBMotor;
+	vex::motor* LFMotor;
+	vex::motor* RBMotor;
+	vex::motor* RFMotor;
+  WheelData wheel_data;
 public:
-	Drive(vex::motor lb_motor, vex::motor lf_motor, vex::motor rb_motor, vex::motor rf_motor);
-	DriveArcade(vex::controller::axis f, vex::controller::axis r);
-	DriveTank(vex::controller::axis l, vex::controller::axis r);
+	void InitDrive(vex::motor lb_motor, vex::motor lf_motor, vex::motor rb_motor, vex::motor rf_motor, WheelData wheel_data);
+  void DriveArcade(vex::controller::axis f, vex::controller::axis r);
+	void DriveTank(vex::controller::axis l, vex::controller::axis r);
+  void DriveForward(float inches, bool coast = false, double pwr = 75);
+  void DriveBackwards(float inches, bool coast = false, double pwr = 75);
+  void Turn(float degrees, double pwr = 75);
+};
+
+void Drive::InitDrive(vex::motor lb_motor, vex::motor lf_motor, vex::motor rb_motor, vex::motor rf_motor, WheelData wheel_data) {
+	this->LBMotor = &lb_motor;
+	this->LFMotor = &lf_motor;
+	this->RBMotor = &rb_motor;
+	this->RFMotor = &rf_motor;
 }
 
-Drive::Drive(vex::motor lb_motor, vex::motor lf_motor, vex::motor rb_motor, vex::motor rf_motor) {
-	this->LBMotor = lb_motor;
-	this->LFMotor = lf_motor;
-	this->RBMotor = rb_motor;
-	this->RFMotor = rf_motor;
+void Drive::DriveArcade(vex::controller::axis f, vex::controller::axis r) {
+  double fwd = f.position();
+  double rot = r.position();
+  LBMotor->spin(directionType::fwd, fwd + rot, velocityUnits::pct);
+  LFMotor->spin(directionType::fwd, fwd + rot, velocityUnits::pct);
+  RBMotor->spin(directionType::fwd, -(fwd - rot), velocityUnits::pct);
+  RFMotor->spin(directionType::fwd, -(fwd - rot), velocityUnits::pct);
+}
+
+void Drive::DriveTank(vex::controller::axis l, vex::controller::axis r) {
+  double left = l.position();
+  double right = r.position();
+  LFMotor->spin(directionType::fwd, left, velocityUnits::pct);
+  LBMotor->spin(directionType::fwd, left, velocityUnits::pct);
+  RFMotor->spin(directionType::fwd, -1 * right, velocityUnits::pct);
+  RBMotor->spin(directionType::fwd, -1 * right, velocityUnits::pct);
+}
+
+void Drive::DriveForward(float inches, bool coast, double pwr) {
+  float wheelCirc =  this->wheel_data.wheel_circumference * PI;
+  float rots = inches / wheelCirc;
+  //resetDriveEnc();
+  LBMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, false);
+  LFMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, false);
+  RBMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, false);
+  RFMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, true);
+  if(!coast) {
+    LBMotor->stop(vex::brakeType::hold);
+    LFMotor->stop(vex::brakeType::hold);
+    RBMotor->stop(vex::brakeType::hold);
+    RFMotor->stop(vex::brakeType::hold);
+  }
+}
+
+void Drive::DriveBackwards(float inches, bool coast, double pwr) {
+  float wheelCirc =  this->wheel_data.wheel_circumference * PI;
+  float rots = inches / wheelCirc * (-1);
+  //resetDriveEnc();
+  LBMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, false);
+  LFMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, false);
+  RBMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, false);
+  RFMotor->rotateFor(rots, rotationUnits::rev, pwr, velocityUnits::pct, true);
+  if(!coast) {
+    LBMotor->stop(vex::brakeType::hold);
+    LFMotor->stop(vex::brakeType::hold);
+    RBMotor->stop(vex::brakeType::hold);
+    RFMotor->stop(vex::brakeType::hold);
+  }
+}
+
+
+// WARNING: untested
+void Drive::Turn(float degrees, double pwr) {
+  // Note: +90 degrees is a right turn
+    float turningRatio = this->wheel_data.wheel_turning_diameter / (this->wheel_data.wheel_circumference / PI);
+    float wheelDegrees = turningRatio * degrees;    
+    // Divide by two because each wheel provides half the rotation
+    LBMotor->startRotateFor(
+        wheelDegrees * this->wheel_data.wheel_gear_ratio / 2, vex::rotationUnits::deg, 
+        pwr, vex::velocityUnits::pct
+    );
+    LFMotor->startRotateFor(
+      wheelDegrees * this->wheel_data.wheel_gear_ratio / 2, vex::rotationUnits::deg, 
+        pwr, vex::velocityUnits::pct
+    );
+    RBMotor->startRotateFor(
+        wheelDegrees * this->wheel_data.wheel_gear_ratio / 2, vex::rotationUnits::deg,
+        pwr * -1, vex::velocityUnits::pct
+    );
+    RFMotor->rotateFor(
+      wheelDegrees * this->wheel_data.wheel_gear_ratio / 2, vex::rotationUnits::deg,
+        pwr * -1, vex::velocityUnits::pct
+    );
 }
 
 #endif // DRIVE_H_FILE
